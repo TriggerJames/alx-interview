4e02c9d157941cb11031abb3c776f650b984cece
TriggerJames <Kimothojames254@gmail.com>
2024-08-19 00:56:54 +0300

    0x01.
    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,15 +7,15 @@ def canUnlockAll(boxes):
     unlocked = set([0])
     keys = set(boxes[0])
     stack = list(keys)
-    
+
     while stack:
         current_box = stack.pop()
-        
+
         if current_box not in unlocked and current_box < len(boxes):
             unlocked.add(current_box)
             for key in boxes[current_box]:
                 if key not in unlocked:
                     stack.append(key)
-    
+
     # Check if we were able to unlock all boxes
     return len(unlocked) == len(boxes)
