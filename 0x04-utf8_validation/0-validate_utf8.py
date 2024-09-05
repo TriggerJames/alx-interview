9dca7d41a496fa326956ee03979e22b7b9e9ffab
TriggerJames <Kimothojames254@gmail.com>
2024-09-05 22:08:53 +0300

    0x04
    UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""UTF-8 Validation"""
+
+
+def validUTF8(data):
+    # Number of bytes in the current UTF-8 character
+    num_bytes = 0
+
+    # Masks to check leading bits
+    mask1 = 1 << 7    # 10000000
+    mask2 = 1 << 6    # 01000000
+
+    for num in data:
+        mask = 1 << 7
+        if num_bytes == 0:
+            # Determine the number of bytes in the UTF-8 character
+            while mask & num:
+                num_bytes += 1
+                mask = mask >> 1
+            
+            if num_bytes == 0:
+                continue
+            
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            # Check that the byte starts with 10xxxxxx
+            if not (num & mask1 and not (num & mask2)):
+                return False
+
+        num_bytes -= 1
+
+    return num_bytes == 0
+
+# Testing the function with given examples
+if __name__ == "__main__":
+    data = [65]
+    print(validUTF8(data))  # True
+
+    data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
+    print(validUTF8(data))  # True
+
+    data = [229, 65, 127, 256]
+    print(validUTF8(data))  # False
