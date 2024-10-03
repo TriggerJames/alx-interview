6d04510dd66330b90541cddef0a0317b1f590d30
TriggerJames <Kimothojames254@gmail.com>
2024-10-03 23:19:55 +0300

    0x08.
    Making Change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,7 +5,6 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    # Initialize dp array with a large number (infinity) for all amounts except 0
     dp = [float('inf')] * (total + 1)
     dp[0] = 0  # Base case: 0 coins are needed to make amount 0
 
