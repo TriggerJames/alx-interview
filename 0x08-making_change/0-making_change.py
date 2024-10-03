c3385a6da9c06d022d2ca40f095671fb1e9b22a8
TriggerJames <Kimothojames254@gmail.com>
2024-10-03 23:23:29 +0300

    0x08.
    Making Change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
+""" 0. Change comes from within """
+
 
 def makeChange(coins, total):
-    # If total is 0 or less, no coins are needed
-    if total <= 0:
+    """ Given a pile of coins of different values, determine the fewest number
+        of coins needed to meet a given amount total.
+    """
+    if total < 1:
         return 0
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # Base case: 0 coins are needed to make amount 0
-
-    # Process each coin denomination
+    coins.sort()
+    coins.reverse()
+    fewest = 0
     for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    # If the total cannot be made with the given coins, return -1
-    return dp[total] if dp[total] != float('inf') else -1
+        if total <= 0:
+            break
+        buff = total // coin
+        fewest += buff
+        total -= (buff * coin)
+    if total != 0:
+        return -1
+    return fewest
