566d899ecc0112473f45ef8b006b5615ea728808
TriggerJames <Kimothojames254@gmail.com>
2024-10-18 00:00:34 +0300

    0x0A.
    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,57 +1,30 @@
 #!/usr/bin/python3
-"""Prime Game"""
-
-
-def sieve_of_eratosthenes(n):
-    """Helper function to generate primes
-    up to n using Sieve of Eratosthenes"""
-    sieve = [True] * (n + 1)
-    sieve[0] = sieve[1] = False
-    for i in range(2, int(n ** 0.5) + 1):
-        if sieve[i]:
-            for j in range(i * i, n + 1, i):
-                sieve[j] = False
-    primes = [i for i in range(2, n + 1) if sieve[i]]
-    return primes
+""" Module for solving prime game question """
 
 
 def isWinner(x, nums):
-    """
-    Determines the winner of the prime game after x rounds.
-    Maria and Ben take turns picking primes.
-    Maria starts first in every round.
-    """
-    if x < 1 or not nums:
+    """function that checks for the winner"""
+    if not nums or x < 1:
         return None
+    max_num = max(nums)
 
-    max_n = max(nums)
-    primes = sieve_of_eratosthenes(max_n)
-
-    # Track the number of wins for Maria and Ben
-    maria_wins = 0
-    ben_wins = 0
-
-    # Play each round
-    for n in nums:
-        primes_in_game = [p for p in primes if p <= n]
-        moves = 0
-
-        while primes_in_game:
-            # Maria starts, removes a prime and all its multiples
-            prime = primes_in_game.pop(0)
-            primes_in_game = [p for p in primes_in_game if p % prime != 0]
-            moves += 1
-
-        # If the number of moves is odd, Maria wins (she made the last move)
-        if moves % 2 == 1:
-            maria_wins += 1
-        else:
-            ben_wins += 1
-
-    # Determine the overall winner
-    if maria_wins > ben_wins:
+    my_filter = [True for _ in range(max(max_num + 1, 2))]
+    for i in range(2, int(pow(max_num, 0.5)) + 1):
+        if not my_filter[i]:
+            continue
+        for j in range(i * i, max_num + 1, i):
+            my_filter[j] = False
+    my_filter[0] = my_filter[1] = False
+    y = 0
+    for i in range(len(my_filter)):
+        if my_filter[i]:
+            y += 1
+        my_filter[i] = y
+    player1 = 0
+    for x in nums:
+        player1 += my_filter[x] % 2 == 1
+    if player1 * 2 == len(nums):
+        return None
+    if player1 * 2 > len(nums):
         return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
-        return None
+    return "Ben"
